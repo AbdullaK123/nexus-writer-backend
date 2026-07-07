28a3a7a070347934a82f084e7a2c38466f32bd42
AbdullaK123 <abdullamaseeh123@gmail.com>
2026-07-07 20:03:26 +0400

    better editor loading skeletons

--- frontend/src/components/chapter/ChapterEditorPage/ChapterEditor/ChapterEditorContent/ChapterEditorContentLoadingSkeleton/ChapterEditorContentLoadingSkeleton.tsx
+++ frontend/src/components/chapter/ChapterEditorPage/ChapterEditor/ChapterEditorContent/ChapterEditorContentLoadingSkeleton/ChapterEditorContentLoadingSkeleton.tsx
@@ -1,27 +1,34 @@
 import { None, Some } from "oxide.ts";
 import { LoadingSkeleton } from "../../../../../common";
-import styles from "./ChapterEditorContentLoadingSkeleton.module.css"
+import contentStyles from "../ChapterEditorContent.module.css";
+import styles from "./ChapterEditorContentLoadingSkeleton.module.css";
 
-
-
-export function ParagraphSkeleton() {
-    return (
-        <div className={styles['paragraph']}>
-            <LoadingSkeleton className={Some(styles['first-line'])} />
-            <LoadingSkeleton className={None} />
-            <LoadingSkeleton className={None} />
-            <LoadingSkeleton className={None} />
-            <LoadingSkeleton className={None} />
-        </div>
-    )
+function ParagraphSkeleton() {
+  return (
+    <div className={styles.group}>
+      <LoadingSkeleton className={Some(`${styles.line} ${styles['w-70']}`)} />
+      <LoadingSkeleton className={None} />
+      <LoadingSkeleton className={None} />
+      <LoadingSkeleton className={None} />
+    </div>
+  );
 }
 
 export function ChapterEditorContentLoadingSkeleton() {
-    return (
-        <div className={styles['paragraphs-container']}>
-            <ParagraphSkeleton />
-            <ParagraphSkeleton />
-            <ParagraphSkeleton />
+  return (
+    <div className={contentStyles['editor-shell']}>
+      <div className={styles.stack}>
+        <div className={styles.group}>
+          <LoadingSkeleton className={Some(`${styles.line} ${styles['w-60']}`)} />
         </div>
-    )
+        <ParagraphSkeleton />
+        <ParagraphSkeleton />
+        <div className={styles.group}>
+          <LoadingSkeleton className={Some(`${styles.line} ${styles['w-90']}`)} />
+          <LoadingSkeleton className={None} />
+          <LoadingSkeleton className={Some(styles['w-80'])} />
+        </div>
+      </div>
+    </div>
+  );
 }
