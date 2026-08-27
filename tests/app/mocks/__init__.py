9085a6397870651d936c76b958c366b23df52120
Abdalla Kayyani <121222897+AbdullaK123@users.noreply.github.com>
2026-08-28 00:40:08 +0400

    test: export controller stubs

---
+++ tests/app/mocks/__init__.py
@@ -0,0 +1,9 @@
+from .auth import StubAuthService
+from .chat import StubChatService
+from .story import StubStoryService
+
+__all__ = [
+    "StubAuthService",
+    "StubChatService",
+    "StubStoryService",
+]
