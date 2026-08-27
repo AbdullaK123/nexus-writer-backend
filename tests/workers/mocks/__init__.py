ce3002575f712a137d70e2b2037d23cdf62baf28
Abdalla Kayyani <121222897+AbdullaK123@users.noreply.github.com>
2026-08-28 00:51:15 +0400

    test: add worker mock package

---
+++ tests/workers/mocks/__init__.py
@@ -0,0 +1,23 @@
+from tests.workers.mocks.services import (
+    FakeAnalyticsService,
+    FakeChapterRepository,
+    FakeChapterService,
+    FakeEmbeddingService,
+    FakeExtractionService,
+    FakePubSub,
+    FakeRedisClient,
+    FakeStoryService,
+    FakeWorker,
+)
+
+__all__ = [
+    "FakeAnalyticsService",
+    "FakeChapterRepository",
+    "FakeChapterService",
+    "FakeEmbeddingService",
+    "FakeExtractionService",
+    "FakePubSub",
+    "FakeRedisClient",
+    "FakeStoryService",
+    "FakeWorker",
+]
